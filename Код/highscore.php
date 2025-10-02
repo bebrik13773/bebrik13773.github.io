b61c0699fe489a891e10ec576d5ba9c6c85fb418
Я бобер <161559883+bebrik13773@users.noreply.github.com>
2025-10-02 08:23:01 +0600

    Delete Код/highscore.php

--- Код/highscore.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = new mysqli('fdb1029.awardspace.net','4567560_db', 'Bopka_01', '4567560_db');
-
-$result = $conn->query("SELECT login, score FROM users ORDER BY score DESC LIMIT 1");
-$highscore = $result->fetch_assoc();
-echo json_encode($highscore);
-?>
