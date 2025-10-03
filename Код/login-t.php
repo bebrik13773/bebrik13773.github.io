31e75453a108487b3b3ca33ecfbcd032d8c093cb
bebrik <161559883+bebrik13773@users.noreply.github.com>
2025-10-03 16:35:36 +0600

    Revert "Добавление параметра ban в login-t.php"
    This reverts commit fabf3b344e9ad460ca6406403de1e7bed9994276.

--- Код/login-t.php
+++ Код/login-t.php
@@ -4,15 +4,15 @@ $data = json_decode(file_get_contents("php://input"));
 $login = $data->login;
 $password = $data->password;
 
-$stmt = $conn->prepare("SELECT id, password, plus, ban, skin, score FROM users WHERE login = ?");
+$stmt = $conn->prepare("SELECT id, password, plus, skin, score FROM users WHERE login = ?");
 $stmt->bind_param("s", $login);
 $stmt->execute();
 $stmt->store_result();
-$stmt->bind_result($id, $hashedPassword, $score, $plus, $skin, $ban);
+$stmt->bind_result($id, $hashedPassword, $score, $plus, $skin);
 $stmt->fetch();
 
 if (password_verify($password, $hashedPassword)) {
-	echo json_encode(['success' => true, 'userId' => $id, 'score' => $score, 'plus' => $plus, 'skin' => $skin, 'ban' => $ban]);
+	echo json_encode(['success' => true, 'userId' => $id, 'score' => $score, 'plus' => $plus, 'skin' => $skin]);
 } else {
     echo json_encode(['success' => false, 'message' => 'Неверный логин или пароль.']);
 }
