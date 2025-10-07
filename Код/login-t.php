b8ff35dc517c7ed74f3ab2262ef4557e45cf224c
bebrik <161559883+bebrik13773@users.noreply.github.com>
2025-10-07 13:06:15 +0600

    Тестовое изменение логин пхп

--- Код/login-t.php
+++ Код/login-t.php
@@ -4,23 +4,23 @@ $data = json_decode(file_get_contents("php://input"));
 $login = $data->login;
 $password = $data->password;
 
-$stmt = $conn->prepare("SELECT id, password, plus, skin, energy, ENERGY_MAX, last_energy_update, score FROM users WHERE login = ?");
+$stmt = $conn->prepare("SELECT id, password, plus, skin, energy, last_energy_update, ENERGY_MAX, score FROM users WHERE login = ?");
 $stmt->bind_param("s", $login);
 $stmt->execute();
 $stmt->store_result();
-$stmt->bind_result($id, $hashedPassword, $plus, $skin, $energy, $lastEnergyUpdate, $score, $ENERGY_MAX);
+$stmt->bind_result($id, $hashedPassword, $plus, $skin, $energy, $lastEnergyUpdate, $ENERGY_MAX, $score);
 $stmt->fetch();
 
 if (password_verify($password, $hashedPassword)) {
 	echo json_encode([
 		'success' => true,
 		'userId' => $id,
-		'score' => $score,
 		'plus' => $plus,
 		'skin' => $skin,
 		'energy' => $energy,
 		'lastEnergyUpdate' => $lastEnergyUpdate,
-		'ENERGY_MAX' => $ENERGY_MAX
+		'ENERGY_MAX' => $ENERGY_MAX,
+		'score' => $score,
 	]);
 } else {
 	echo json_encode(['success' => false, 'message' => 'Неверный логин или пароль.']);
